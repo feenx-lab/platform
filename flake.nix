95d1f43d2f4b5afd06dc1af3ae21cbaf52eeedfc
azunaVT <36605286+azunaVT@users.noreply.github.com>
2025-06-30 04:31:44 +0000

    Renamed production to prod and staging to dev

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "feenx-platform";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            kubectl
+            kubernetes-helm
+
+            # Alias scripts, workaround for https://github.com/direnv/direnv/issues/73
+            (pkgs.writeShellScriptBin "k" "kubectl $@")
+            (pkgs.writeShellScriptBin "h" "helm $@")
+          ];
+        };
+      }
+    );
+}
